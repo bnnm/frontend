926ca899549910ceb84daa9c213dda22e8747311
bnnm <bananaman255@gmail.com>
2022-12-08 10:51:59 +0100

    ignore index.json since index-clean.json is now used

--- .updater.sh
+++ .updater.sh
@@ -1,17 +1,17 @@
-# index.json updater
+# index.json updater (run by github's cron every now and then)
 #
-# We want to get latest .json only if file has changed in server. This is handled by wget
+# We want to get latest .json only if file has changed in joshw's server. This is handled by wget
 # ("wget --timestamp (url)") as long as file's timestamp is older than server's.
 #
-# Github's workflows redownload everything and git doesn't save timestamps though, so we could check
-# a file's latest commit ("git log (file)") and apply it ("touch -d (format) (file)".
-# However commit info may only exist when fetching all commits in the workflow (using fetch-depth: 0),
-# otherwise would only get latest commit (from any file).
+# Github's workflows however redownload everything from repo, and git doesn't save timestamps though, so
+# repo's index.json has no date to compare. We could check file's latest commit ("git log (file)") and
+# apply it ("touch -d (format) (file)". However commit info may only exist when fetching all commits in
+# the workflow (using fetch-depth: 0), otherwise would only get latest commit from any file.
 #
-# Could also to redownload the file every time ("wget -q -O $INDEX_JSON $INFO_JSON_URL"), as it will be
+# Could also redownload index.json every time ("wget -q -O $INDEX_JSON $INFO_JSON_URL"), as would be
 # ignored by "git commit"), but it's not very gentle to the server.
 #
-# Instead have a text file with the file's last update timestamp and apply that, easier to setup/test/modify.
+# Instead we have a text file with the file's last update timestamp and apply that, easier to setup/test/modify.
 
 # env vars:
 INFO_JSON_URL=$1
@@ -21,10 +21,10 @@ INDEX_JSON=index.json
 INDEXC_JSON=index-clean.json
 INDEX_VERSION=index.version
 
-# get version from file
+# get version from helper file
 VERSION=$(cat $INDEX_VERSION)
 
-# same, from commit (needs full repo download)
+# same from commit but needs a full repo download = worse
 #VERSION=$(git log --pretty=format:%cI "$INDEX_JSON")
 
 touch -am -c -d $VERSION "$INDEX_JSON"
@@ -39,7 +39,7 @@ if [ "$CHANGED" != "$INDEX_JSON" ]; then
     exit 0
 fi
 
-# clean list for smaller sizes
+# clean index.json for smaller sizes
 python3 ./.index-cleaner.py
 
 
@@ -48,7 +48,7 @@ NEW_VERSION=$(date +%Y-%m-%dT%T%z)
 
 echo "$INDEX_JSON: updated (old: $VERSION, new: $NEW_VERSION)"
 
-# may be needed by github actions
+# may be needed by github actions in order to commit below
 if [ ! -z "$USER_MAIL" ]; then
     git config user.email "$USER_MAIL"
 fi
@@ -58,11 +58,16 @@ fi
 
 echo $NEW_VERSION > $INDEX_VERSION
 
-# commit changed files (if they actually changed) so they are deployed
-git add "$INDEX_JSON"
-git add "$INDEXC_JSON"
+# commit changed files (ignored if not actually changed), so site is re-deployed by github's bots
 git add "$INDEX_VERSION"
+git add "$INDEXC_JSON"
+# no need to update index.json as timestamp is in index.version and generated index-clean.json
+# is now used and updated instead (unused index.json would be deployed as well, bloating the site)
+#git add "$INDEX_JSON"
+
 git commit -m "update sets"
 git push
+
+# 
 
 exit 0
