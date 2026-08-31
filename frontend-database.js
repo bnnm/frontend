09a386754f2379a5c05378ac816e04797cab41b0
bnnm <bananaman255@gmail.com>
2026-08-31 02:24:51 +0200

    fix exts with spaces in filelists

--- frontend-database.js
+++ frontend-database.js
@@ -487,6 +487,7 @@ class FilelistSetup {
             // after loading dir/name
             let ext = this._extract_ext(set, file);
             ext = ext.toLowerCase();
+            ext = ext.replace(/\s/g, '_'); // spaces in exts affect stuff
             file.ext = ext;
 
             if (!filelist.extensions.includes(ext))
